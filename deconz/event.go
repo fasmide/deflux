74e802fc3aef22edd64e274ff9732e48e7c4109f
Kristian Mide <fas@mide.dk>
2018-03-20 22:27:54 +0100

    New hopefully more robost parsing of events

--- deconz/event.go
+++ deconz/event.go
@@ -1,17 +1,90 @@
 package deconz
 
 import (
-	"bytes"
 	"encoding/json"
-	"errors"
+	"fmt"
 )
+
+// TypeStore is used to lookup what event to parse from an device ID
+var TypeStore TypeLookuper
+
+// TypeLookuper is the interface that we require to lookup types from id's
+type TypeLookuper interface {
+	LookupType(int) (string, error)
+}
 
 // Event represents a deconz sensor event
 type Event struct {
-	Type     string `json:"t"`
-	Event    string `json:"e"`
-	Resource string `json:"r"`
-	ID       string `json:"id"`
+	Type     string          `json:"t"`
+	Event    string          `json:"e"`
+	Resource string          `json:"r"`
+	ID       int             `json:"id,string"`
+	RawState json.RawMessage `json:"state"`
+	State    interface{}
+}
+
+// Parse parses events from bytes
+func Parse(b []byte) (*Event, error) {
+	var e Event
+	err := json.Unmarshal(b, &e)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal event: %s", err)
+	}
+
+	err = e.ParseState()
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal event: %s", err)
+	}
+
+	return &e, nil
+}
+
+// ParseState tries to unmarshal the appropriate state based
+// on looking up the id though the TypeStore
+func (e *Event) ParseState() error {
+
+	t, err := TypeStore.LookupType(e.ID)
+	if err != nil {
+		return fmt.Errorf("unable to lookup event id %d: %s", e.ID, err)
+	}
+
+	switch t {
+	case "ZHAFire":
+		var s ZHAFire
+		err = json.Unmarshal(e.RawState, &s)
+		e.State = s
+		break
+	case "ZHATemperature":
+		var s ZHATemperature
+		err = json.Unmarshal(e.RawState, &s)
+		e.State = s
+		break
+	case "ZHAPressure":
+		var s ZHAPressure
+		err = json.Unmarshal(e.RawState, &s)
+		e.State = s
+		break
+	case "ZHAHumidity":
+		var s ZHAHumidity
+		err = json.Unmarshal(e.RawState, &s)
+		e.State = s
+		break
+	case "ZHAWater":
+		var s ZHAWater
+		err = json.Unmarshal(e.RawState, &s)
+		e.State = s
+		break
+	case "ZHASwitch":
+		var s ZHASwitch
+		err = json.Unmarshal(e.RawState, &s)
+		e.State = s
+		break
+	default:
+		err = fmt.Errorf("unable to unmarshal event state: %s is not a known type", t)
+	}
+
+	// err should continue to be null if everythings ok
+	return err
 }
 
 // State is for embedding into event states
@@ -19,112 +92,42 @@ type State struct {
 	Lastupdated string
 }
 
-// HumidityEvent represents a presure change
-type HumidityEvent struct {
-	Event
-	State struct {
-		State
-		Humidity int
-	}
+// ZHAHumidity represents a presure change
+type ZHAHumidity struct {
+	State
+	Humidity int
 }
 
-// PressureEvent represents a presure change
-type PressureEvent struct {
-	Event
-	State struct {
-		State
-		Pressure int
-	}
+// ZHAPressure represents a presure change
+type ZHAPressure struct {
+	State
+	Pressure int
 }
 
-// TemperatureEvent represents a temperature change
-type TemperatureEvent struct {
-	Event
-	State struct {
-		State
-		Temperature int
-	}
+// ZHATemperature represents a temperature change
+type ZHATemperature struct {
+	State
+	Temperature int
 }
 
-// FloodEvent respresents an event from a flood sensor
-type FloodEvent struct {
-	Event
-	State struct {
-		State
-		Lowbattery bool
-		Tampered   bool
-		Water      bool
-	}
+// ZHAWater respresents a change from a flood sensor
+type ZHAWater struct {
+	State
+	Lowbattery bool
+	Tampered   bool
+	Water      bool
 }
 
-// SmokeDetectorEvent resporesents an event from a smoke detector
-type SmokeDetectorEvent struct {
-	Event
-	State struct {
-		State
-		Fire       bool
-		Lowbattery bool
-		Tampered   bool
-	}
+// ZHAFire represents a change from a smoke detector
+type ZHAFire struct {
+	State
+	Fire       bool
+	Lowbattery bool
+	Tampered   bool
 }
 
-// Unmarshal decodes and returns apporiate event
-// TODO: This should at least be made more robust, for example
-// passing {e: "something"} will make unmarshal return an somewhat
-// faulty TemperatureEvent...
-// this is not really what we want when dealing with
-// something like a smoke detector
-func Unmarshal(payload []byte) (interface{}, error) {
-
-	// in order to use DisallowUnknownFields we cannot use
-	// the typical json.Unmarshal we must create a decoder
-	// in order to pass the same payload multiple times, we
-	// create this buffer and write the payload again as we
-	// along...
-	buf := bytes.NewBuffer(payload)
-
-	dec := json.NewDecoder(buf)
-	dec.DisallowUnknownFields()
-
-	var a TemperatureEvent
-	err := dec.Decode(&a)
-	if err == nil {
-		return &a, nil
-	}
-
-	// if the above failed, we must rewrite the same byte slice
-	// otherwise the decoder will just read EOF
-	buf.Write(payload)
-
-	var b PressureEvent
-	err = dec.Decode(&b)
-	if err == nil {
-		return &b, nil
-	}
-
-	buf.Write(payload)
-
-	var c HumidityEvent
-	err = dec.Decode(&c)
-	if err == nil {
-		return &c, nil
-	}
-
-	buf.Write(payload)
-
-	var d FloodEvent
-	err = dec.Decode(&d)
-	if err == nil {
-		return &d, nil
-	}
-
-	buf.Write(payload)
-
-	var e SmokeDetectorEvent
-	err = dec.Decode(&e)
-	if err == nil {
-		return &e, nil
-	}
-
-	return nil, errors.New("payload did not match any of our event types")
+// ZHASwitch represents a change from a button or switch
+type ZHASwitch struct {
+	State
+	Buttonevent int
 }
--- deconz/event_test.go
+++ deconz/event_test.go
@@ -1,6 +1,8 @@
 package deconz
 
 import (
+	"errors"
+	"os"
 	"testing"
 )
 
@@ -15,29 +17,54 @@ const smokeDetectorNoFireEventPayload = `{	"e": "changed",	"id": "5",	"r": "sens
 // xiaomi flood detector
 const floodDetectorFloodDetectedEventPayload = `{ "e": "changed", "id": "6", "r": "sensors", "state": { "lastupdated": "2018-03-13T20:46:03", "lowbattery": false, "tampered": false, "water": true }, "t": "event"   }`
 
+// xiaomi random switch "sensor"
+const switchSensorEventPayload = `{	"e": "changed",	"id": "7",	"r": "sensors",	"state": {	  "buttonevent": 1000,	  "lastupdated": "2018-03-20T20:52:18"	},	"t": "event"  }  `
+
 // some unknown json
 const unknownEventPayload = `{"e": "hello my friend", "y": "Pænt go dag Hr 🦆"}`
 
+type LookupImpl struct {
+	Store map[int]string
+}
+
+func (l *LookupImpl) LookupType(i int) (string, error) {
+	if t, ok := l.Store[i]; ok {
+		return t, nil
+	}
+	return "", errors.New("not found")
+}
+
+func TestMain(m *testing.M) {
+	TypeStore = &LookupImpl{Store: map[int]string{
+		1: "ZHATemperature",
+		2: "ZHAHumidity",
+		3: "ZHAPressure",
+		5: "ZHAFire",
+		6: "ZHAWater",
+		7: "ZHASwitch",
+	}}
+	os.Exit(m.Run())
+}
 func TestSmokeDetectorNoFireEvent(t *testing.T) {
-	result, err := Unmarshal([]byte(smokeDetectorNoFireEventPayload))
+	result, err := Parse([]byte(smokeDetectorNoFireEventPayload))
 	if err != nil {
 		t.Logf("unable to unmarshal smoke detector event: %s", err)
 		t.FailNow()
 	}
 
-	smokeDetectorEvent, success := result.(*SmokeDetectorEvent)
+	smokeDetectorEvent, success := result.State.(ZHAFire)
 	if !success {
 		t.Log("unable to type assert smoke detector event")
 		t.FailNow()
 	}
 
-	if smokeDetectorEvent.State.Fire != false {
+	if smokeDetectorEvent.Fire != false {
 		t.Fail()
 	}
 }
 
 func TestUnknownEvent(t *testing.T) {
-	_, err := Unmarshal([]byte(unknownEventPayload))
+	_, err := Parse([]byte(unknownEventPayload))
 	if err == nil {
 		t.Fail()
 	}
@@ -45,19 +72,19 @@ func TestUnknownEvent(t *testing.T) {
 
 func TestFloodDetectorEvent(t *testing.T) {
 
-	result, err := Unmarshal([]byte(floodDetectorFloodDetectedEventPayload))
+	result, err := Parse([]byte(floodDetectorFloodDetectedEventPayload))
 	if err != nil {
 		t.Logf("Could not parse flood detector event: %s", err)
 		t.FailNow()
 	}
 
-	floodEvent, success := result.(*FloodEvent)
+	floodEvent, success := result.State.(ZHAWater)
 	if !success {
 		t.Log("Unable to type assert floodevent")
 		t.FailNow()
 	}
 
-	if !floodEvent.State.Water {
+	if !floodEvent.Water {
 		t.Fail()
 	}
 
@@ -65,58 +92,76 @@ func TestFloodDetectorEvent(t *testing.T) {
 
 func TestPressureEvent(t *testing.T) {
 
-	result, err := Unmarshal([]byte(pressureEventPayload))
+	result, err := Parse([]byte(pressureEventPayload))
 	if err != nil {
 		t.Logf("Could not parse pressure: %s", err)
 		t.FailNow()
 	}
 
-	pressure, success := result.(*PressureEvent)
+	pressure, success := result.State.(ZHAPressure)
 	if !success {
 		t.Log("Coudl not assert to pressureevent")
 		t.FailNow()
 	}
 
-	if pressure.State.Pressure != 993 {
+	if pressure.Pressure != 993 {
 		t.Fail()
 	}
 }
 
 func TestTemperatureEvent(t *testing.T) {
 
-	result, err := Unmarshal([]byte(temperatureEventPayload))
+	result, err := Parse([]byte(temperatureEventPayload))
 	if err != nil {
 		t.Logf("Could not parse temperature: %s", err)
 		t.FailNow()
 	}
 
-	temp, success := result.(*TemperatureEvent)
+	temp, success := result.State.(ZHATemperature)
 	if !success {
 		t.Logf("Could not assert to temperature event")
 		t.FailNow()
 	}
 
-	if temp.State.Temperature != 2062 {
+	if temp.Temperature != 2062 {
 		t.Fail()
 	}
 }
 
 func TestHumidityEvent(t *testing.T) {
 
-	result, err := Unmarshal([]byte(humidityEventPayload))
+	result, err := Parse([]byte(humidityEventPayload))
 	if err != nil {
 		t.Logf("Could not parse humidity: %s", err)
 		t.FailNow()
 	}
 
-	humidity, success := result.(*HumidityEvent)
+	humidity, success := result.State.(ZHAHumidity)
 	if !success {
 		t.Logf("unable assert humidity event")
 		t.FailNow()
 	}
 
-	if humidity.State.Humidity != 2985 {
-		t.Logf("unexpected humidity value %d", humidity.State.Humidity)
+	if humidity.Humidity != 2985 {
+		t.Fail()
+	}
+}
+
+func TestSwitchEvent(t *testing.T) {
+
+	result, err := Parse([]byte(switchSensorEventPayload))
+	if err != nil {
+		t.Logf("Could not parse switch event: %s", err)
+		t.FailNow()
+	}
+
+	s, success := result.State.(ZHASwitch)
+	if !success {
+		t.Logf("unable assert switch event")
+		t.FailNow()
+	}
+
+	if s.Buttonevent != 1000 {
 		t.Fail()
 	}
 }
